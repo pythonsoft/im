70fbdcc33966cb34212e479abcf67135f7bd6feb
谢朝宁 <chaoningx@163.com>
2017-11-29 10:15:58 +0800

    Merge branch 'master' of http://gitlab.szdev.cn/chaoningx/im
    # Conflicts:
    #	src/server/middleware/login.js

--- src/server/config_master.js
+++ src/server/config_master.js
@@ -4,12 +4,15 @@ config.domain = `http://${config.host}`;
 config.dbName = 'im';
 
 config.mongodb = {
+  // [`${config.dbName}URL`]: `mongodb://localhost:27017/${config.dbName === 'ump' ? 'ump_v1' : 'ump_test'}`,
   [`${config.dbName}URL`]: `mongodb://10.0.15.62:27017/${config.dbName === 'im' ? 'im_v1' : 'im_test'}`,
 };
 
 config.redis_host = '10.0.15.69';
+// config.redis_host = 'localhost';
 config.redis_port = 6379;
-config.redis_opts = { auth_pass: 'phoenixtv2017' };
+// config.redis_opts = { auth_pass: 'phoenixtv2017' };
+config.redis_opts = { auth_pass: '' };
 
 // 日志路径
 config.logPath = path.join(__dirname, '../logs/');
